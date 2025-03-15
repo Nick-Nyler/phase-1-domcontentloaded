c1b2f76a1cf3dc6ed382725259000a191cf2d2f5
Nick <nicknyler@gmail.com>
2025-03-15 21:51:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.getElementById('text'); 
+    element.textContent = 'This is really cool!';
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
